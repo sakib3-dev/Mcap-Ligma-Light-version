123e4b802fb97e28d384b3c706c3716930557cfe
Sakib-Dev <139985754+sakib3-dev@users.noreply.github.com>
2023-08-13 23:15:19 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// mixitup//
+
+var containerEl = document.querySelector('.mixitup_filter');
+
+ var mixer = mixitup(containerEl);
+
+
+// mixitup//
+
+
+// slick slider //
+
+$('.autoplay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows:false,
+  });
+
+// slick slider //
+
